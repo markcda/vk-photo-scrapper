3ce957230b02094ac268519ad3108e700a62428f
kollieartwolf <titoffklim@cclc.tech>
2020-05-31 03:01:03 +0300

    Create vk-photo-scrapper.py

---
+++ vk-photo-scrapper.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import webbrowser
+import requests
+import os.path
+
+def getFile(filename):
+    return open(filename)
+
+def getLinks(f):
+    links = []
+    for line in f:
+        if (found := line.find('https://')) != -1:
+            links.append(line[found : ])
+    total = len(links)
+    print(f'Скрипт нашёл {total} фотографий в файле.')
+    return links
+
+def openLinks(links):
+    for link_index in range(len(links)):
+        try:
+            if os.path.exists(f'{link_index}.jpg'):
+                continue
+            f = open(f'{link_index}.jpg',"wb")
+            ufr = requests.get(links[link_index])
+            f.write(ufr.content)
+            f.close()
+        except:
+            print('Сайт/ссылка недоступна.')
+    print('Все файлы выведены.')
+    return True
+
+def main():
+    return openLinks(getLinks(getFile(input('Введите имя файла: '))))
+    
+if __name__ == '__main__':
+    main()
